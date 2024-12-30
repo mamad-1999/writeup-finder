7a9c9a8bfe07545c6e1c0ef6a6d5c8297f92a74c
mamad <www.mmhmdmm83@gmail.com>
2024-12-30 21:20:33 +0330

    feat: add check windscribe Vpn

--- run_writeUp-finder.sh
+++ run_writeUp-finder.sh
@@ -4,6 +4,7 @@ source /home/mohammad/Videos/go/proxy.env
 
 # Define variables
 SCRIPT_PATH="$HOME/Videos/go/writeup-finder" # Change this to your actual script directory
+
 # Function to check proxy connection
 check_proxy() {
     nc -zv "$PROXY_HOST" "$PROXY_PORT" >/dev/null 2>&1
@@ -14,15 +15,28 @@ check_proxy() {
     fi
 }
 
-# Now, check the proxy connection
-if check_proxy; then
-    echo "Proxy is up, running the writeup-finder script..."
+# Function to check if Windscribe VPN is up
+check_windscribe() {
+    windscribe-cli status | grep -q "Connected"
+    if [ $? -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-    # Run the barcelona-watch script with the proxy
+# Main logic
+if check_windscribe; then
+    echo "Windscribe VPN is up, running the writeup-finder script without proxy..."
     cd "$SCRIPT_PATH" || { echo "Failed to change directory to $SCRIPT_PATH"; exit 1; }
-    
+    $HOME/Videos/go/writeup-finder/writeup-finder --database --telegram
+
+elif check_proxy; then
+    echo "Proxy is up, running the writeup-finder script with proxy..."
+    cd "$SCRIPT_PATH" || { echo "Failed to change directory to $SCRIPT_PATH"; exit 1; }
     $HOME/Videos/go/writeup-finder/writeup-finder --database --telegram --proxy="$PROXY"
 
 else
-    echo "Proxy is down, skipping this attempt."
+    echo "Neither Windscribe VPN nor proxy is available. Skipping this attempt."
 fi
+
