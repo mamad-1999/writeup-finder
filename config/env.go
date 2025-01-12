dd8db7b709999f988dee0b6649776396a17a89a9
mamad <www.mmhmdmm83@gmail.com>
2025-01-12 20:29:49 +0330

    chore: add some changes in path of .env and workflows for Action

--- config/env.go
+++ config/env.go
@@ -1,12 +1,18 @@
 package config
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/joho/godotenv"
 	"writeup-finder.go/utils"
 )
 
 func LoadEnv() {
-	err := godotenv.Load("/home/mohammad/Videos/go/writeup-finder/.env")
+	// err := godotenv.Load("/home/mohammad/Videos/go/writeup-finder/.env")
+
+	envFile := filepath.Join(os.Getenv("GITHUB_WORKSPACE"), ".env")
+	err := godotenv.Load(envFile)
 
 	utils.HandleError(err, "Error loading .env file:", true)
 }
