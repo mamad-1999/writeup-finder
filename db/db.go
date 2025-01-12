524f089f0202297f1c89dbefc05c89e238131f00
mamad <www.mmhmdmm83@gmail.com>
2025-01-12 20:46:12 +0330

    Test: For Action

--- db/db.go
+++ db/db.go
@@ -11,7 +11,7 @@ import (
 )
 
 func ConnectDB() *sql.DB {
-	connStr := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+	connStr := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=require",
 		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"),
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
 
